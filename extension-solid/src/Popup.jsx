bd582e2f4136a80166843601652d700bd03b2102
Xavier Ang <xaveang@gmail.com>
2025-02-26 18:14:06 +0800

    Implement Popup

--- extension-solid/src/Popup.jsx
+++ extension-solid/src/Popup.jsx
@@ -1,13 +1,34 @@
-import { onMount } from 'solid-js';
+import { onMount, createEffect } from 'solid-js';
 
-function Popup() {
-    onMount(() => {
-        const id = document.getElementById('popup');
-        console.log(id);
+function Popup(props) {
+    const pos = props.pos;
+    const isLink = props.isLink;
+    const id = props.id;
+
+    createEffect(() => {
+        if (isLink()) {
+            const popup = document.getElementById(id);
+            console.log(popup);
+        }
     });
 
     return (
-        <div id="popup" class="absolute bg-black">hi</div>
+        <div id={props.id}
+            style={{
+                display: isLink() ? 'block' : 'none',
+                position: 'absolute',
+                top: pos.top + pos.height * 0.66 +'px',
+                left: pos.left + 20 + 'px',
+                'max-width': window.innerWidth - pos.left - 50 + 'px',
+            }}
+            className="p-2 bg-[#F2F0E4] h-fit rounded-xl break-words shadow-xl"
+        >
+            <p>{props.popupText()}</p>
+            <div className="mt-4 mb-2 flex w-full space-x-2 justify-end">
+                <button className="py-1 px-3 rounded-full">Cancel</button>
+                <button className="py-1 px-3 bg-[#DD8888] rounded-full">Open</button>
+            </div>
+        </div>
     )
 }
 
--- extension-solid/src/Tracer.jsx
+++ extension-solid/src/Tracer.jsx
@@ -1,17 +1,21 @@
-import { Show, createEffect } from 'solid-js';
+import { createEffect } from 'solid-js';
 
 function Tracer(props) {
     const pos = props.pos;
     const isLink = props.isLink;
-    // console.log(isLink);
 
     return (
-        <Show when={isLink()}>
-            <div id={props.id}
-                style={{ position: 'absolute', top: pos.top +'px', left: pos.left + 'px', width: pos.width + 'px', height: pos.height + 'px' }}
-                class="bg-red-700 opacity-50"
-            ></div>
-        </Show>
+        <div id={props.id}
+            style={{
+                display: isLink() ? 'block' : 'none',
+                position: 'absolute',
+                top: pos.top +'px',
+                left: pos.left + 'px',
+                width: pos.width + 'px',
+                height: pos.height + 'px'
+            }}
+            className="bg-red-700 opacity-30"
+        ></div>
     )
 }
 
