3b3129a7f3e54ae8e662f83e20f6e54d3cb16e3c
Xavier Ang <xaveang@gmail.com>
2025-03-01 12:53:26 +0800

    Add Typescript support

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -17,6 +17,6 @@ export default defineConfig({
             assetFileNames: (assetInfo) => '[name][extname]'        }
     },
     outDir: 'unpacked/assets/',
-    target: 'modules',
+    target: 'esnext',
   },
 });
