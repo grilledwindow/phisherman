ef68476424b4bb1b21660b57f00358ed1b9118fe
Xavier Ang <xaveang@gmail.com>
2025-03-11 21:56:42 +0800

    Add warning icon

--- extension/src/svg/icons.tsx
+++ extension/src/svg/icons.tsx
@@ -5,7 +5,9 @@ export const ErrorCircle = (props: SvgProps) => {
 }
 
 export const CheckCircle = (props: SvgProps) => {
-    console.log(props.fill);
     return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={{ fill: props.fill, transform: ';msFilter:' }}><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm-1.999 14.413-3.713-3.705L7.7 11.292l2.299 2.295 5.294-5.294 1.414 1.414-6.706 6.706z"></path></svg>
 }
 
+export const WarningCone = (props: SvgProps) => {
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={{ fill: props.fill, transform: ';msFilter:' }}><path d="M12.884 2.532c-.346-.654-1.422-.654-1.768 0l-9 17A.999.999 0 0 0 3 21h18a.998.998 0 0 0 .883-1.467L12.884 2.532zM13 18h-2v-2h2v2zm-2-4V9h2l.001 5H11z"></path></svg>
+}
