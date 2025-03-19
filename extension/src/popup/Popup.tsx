d7f2ad656e9f04c1902cea2a16a8430327ba386a
Xavier Ang <xaveang@gmail.com>
2025-03-19 16:39:21 +0800

    Popup: add toggle

--- extension/src/popup/Popup.tsx
+++ extension/src/popup/Popup.tsx
@@ -1,5 +1,6 @@
 import { For, Show } from "solid-js";
 import { WarningCone } from "../svg/icons";
+import Toggle from "./Toggle";
 
 export type PopupStore = {
     show: boolean,
@@ -14,7 +15,7 @@ export function Popup(props: { store: PopupStore }) {
 
     return (
         <div 
-            class="min-w-[300px] h-fit p-3 bg-[#e5e5e5] text-[#444] rounded-xl overflow-hidden drop-shadow-2xl shadow-lg"
+            class="min-w-[320px] h-fit p-3 bg-[#e5e5e5] text-[#444] rounded-xl overflow-hidden drop-shadow-2xl shadow-lg"
             style={{
                 position: 'fixed',
                 display: store.show ? 'block' : 'none',
@@ -23,15 +24,10 @@ export function Popup(props: { store: PopupStore }) {
                 right: '1rem',
             }}
         >
-            <h1 class="text-xl font-semibold">Phisherman</h1>
-            <div class="mt-2">
-                <input type="checkbox" name="Toggle extension" id="toggle-extension"
-                    checked={store.enabled()}
-                    class="h-3 w-3 mr-2 appearance-none outline-none rounded-full cursor-pointer ring-[1.5px] ring-[#555] checked:bg-[#555]"
-                    on:change={(e) => store.onToggleExtension(e.currentTarget.checked)}
-                />
-                <label for="toggle-extension">Enable extension</label>
-            </div>
+            <span class="text-xl align-bottom font-semibold mr-2">
+                Phisherman 
+                <Toggle checked={store.enabled()} onToggle={(e) => store.onToggleExtension(e.currentTarget.checked)} />
+            </span>
 
             {/* email findings: warnings, errors */}
             <Show when={store.enabled()}>
