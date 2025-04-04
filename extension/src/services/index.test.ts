ebc43c6c17d9a7d76d8b37f7bb7bee567da4baf2
Xavier Ang <xaveang@gmail.com>
2025-04-04 19:10:03 +0800

    runTests in test file

--- extension/src/services/index.test.ts
+++ extension/src/services/index.test.ts
@@ -1,9 +1,13 @@
 import { describe, it } from 'vitest';
-import { extractMainDomain } from '.';
+import { extractMainDomain, runTests } from '.';
 
 describe.concurrent('main', () => {
     it('extracts main domain', ({ expect }) => {
         const result = extractMainDomain('https://www.phisherman.com');
         expect(result).toBe('phisherman.com');
+    });
+
+    it('runs tests', () => {
+        runTests();
     });
 });
